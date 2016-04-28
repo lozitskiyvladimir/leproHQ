819f890fcd2b5e7a66d2ba826f10a21c68a2203d
Vladimir Lozitskiy <loza-w@mail.ru>
2016-04-28 10:20:05 +0300

    initial commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+get '/new' do
+  erb "Hello World"
+end
+
+get '/posts' do
+  erb "Hello World"
+end
+
+get '/main' do
+  erb "Hello World"
+end
